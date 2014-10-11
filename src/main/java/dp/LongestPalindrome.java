bbeac16d1564616ca81677fe9827d4742065c328
Sajit Kunnumkal <skunnumkal@amplify.com>
2014-10-11 19:02:09 -0400

    W

--- src/main/java/dp/LongestPalindrome.java
+++ src/main/java/dp/LongestPalindrome.java
@@ -8,21 +8,23 @@ public class LongestPalindrome {
     public static int longestPalLength(String str){
         int[][] table = new int[str.length()][str.length()];
 
+        //all individual chars have length 1
         for(int i=0;i<table.length;i++){
             table[i][i] = 1;
         }
 
-        for(int i=0;i<str.length()-1;i++){
-            if(str.charAt(i) == str.charAt(i+1)){
-                table[i][i+1] = 2;
-            }
-            else{
-                table[i][i+1]=0;
-            }
-        }
+        //adjacent characters should have 2
+//        for(int i=0;i<str.length()-1;i++){
+//            if(str.charAt(i) == str.charAt(i+1)){
+//                table[i][i+1] = 2;
+//            }
+//            else{
+//                table[i][i+1]=0;
+//            }
+//        }
         int max = 0;
-        for(int i=table.length-3;i>=0;i--){
-            for(int j=i+2;j<table[i].length;j++){
+        for(int i=table.length-2;i>=0;i--){
+            for(int j=i+1;j<table[i].length;j++){
                 if(str.charAt(i) == str.charAt(j)){
                     table[i][j] = table[i+1][j-1]+2;
                     if(table[i][j] > max){
@@ -40,5 +42,12 @@ public class LongestPalindrome {
 
         return max;
 
+    }
+
+    public static void main(String[] args){
+        String s1 = "malayalam";
+        String s2 = "mlayalam";
+        System.out.println(LongestPalindrome.longestPalLength(s1));
+        System.out.println(LongestPalindrome.longestPalLength(s2));
     }
 }
