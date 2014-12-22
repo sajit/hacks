93e2f494d9d251fb0c01b711c1ba703325c8f0e0
Sajit Kunnumkal <skunnumkal@amplify.com>
2014-12-22 15:21:54 -0500

    OO design with UML

---
+++ src/main/java/oop/elevator/Button.java
@@ -0,0 +1,11 @@
+package oop.elevator;
+
+import java.util.Date;
+
+/**
+ * Created by skunnumkal on 12/22/14.
+ */
+public class Button {
+    private boolean isPressed = false;
+    private Date pressedTimeStmp;
+}
---
+++ src/main/java/oop/elevator/Elevator.java
@@ -0,0 +1,17 @@
+package oop.elevator;
+
+/**
+ * Created by skunnumkal on 12/22/14.
+ */
+public class Elevator {
+    private Integer floor = 0;
+
+
+    public void moveTo(Integer targetFloor){
+
+    }
+
+    public Integer getCurrentFloor() {
+        return floor;
+    }
+}
