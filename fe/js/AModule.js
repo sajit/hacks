221efcc3a0335bafd4db5d22ecfb645b956cc748
Sajit <sajit.kunnumkal@gmail.com>
2015-05-23 17:08:30 -0400

    Example of script loading and isolation

--- fe/js/AModule.js
+++ fe/js/AModule.js
@@ -5,14 +5,29 @@
 var MODULE = (function () {
     // ... all vars and functions are in this scope only
     // still maintains access to all globals
-    console.log('Jesus please forgive me:: A module');
+    console.log('Jesus please forgive me:: AModule');
 }());
 var CROSS_MODULE = (function(my){
-    console.log('In cross module');
+    console.log('In cross module , execution');
     my.property = 1;
+    var type;
     my.init = function(initObj){
+      type = initObj.type;
+        function reqListener () {
+            //console.log(this.responseText);
+            var sailthruJquery = eval(this.responseText);
+            console.log($('#praiseJesus').length);
+        }
 
+        var oReq = new XMLHttpRequest();
+        oReq.onload = reqListener;
+        var url = "http://code.jquery.com/jquery-2.1.4.min.js";
+        oReq.open("get", url, true);
+        oReq.send();
     };
+    my.getType = function(){
+        return type;
+    }
 
     return my;
 })(CROSS_MODULE || {});
--- fe/js/ClientModule.js
+++ fe/js/ClientModule.js
@@ -1,6 +1,6 @@
 /**
  * Created by sajit on 5/22/15.
  */
-console.log('In client module');
-console.log(MODULE, ' Basic module defined in AModule wont be available in ClientModule');
-console.log(CROSS_MODULE,' This is Cross Module. It should be defined across files');
+var cmProperty = CROSS_MODULE.property;
+CROSS_MODULE.init({type : 'ordinary'});
+console.log(CROSS_MODULE.getType());
