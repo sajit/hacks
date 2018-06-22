1c6b13d034496d1d3d7d4595567f90057e13e5e1
sajit <sajit.kunnumkal@verato.com>
2018-06-21 22:33:51 -0400

    all tours

--- src/main/java/recursion/HorseMan.java
+++ src/main/java/recursion/HorseMan.java
@@ -2,53 +2,36 @@ package recursion;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 
 /**
  * Created by skunnumkal on 10/31/14.
  */
 public class HorseMan {
 
-    private final int size = 8;
-    private int[][] visitCount = new int[size][size];
-    private Cell[][] board = new Cell[size][size];
-    public HorseMan() {
-        for(int i=0;i<size;i++){
-            for(int j=0;j<size;j++){
-                board[i][j] = new Cell(i,j);
-            }
-        }
-    }
+    private static final int size = 8;
+
+
     public static int count = 0;
-    public boolean isTourComplete(){
-        for(int i=0;i<board.length;i++)
-            for(int j=0;j<board[i].length;j++){
-                if(!board[i][j].visited){
-                    return false;
-                }
-            }
+    public boolean traverse(int x,int y,int moveCount,int[][] visitCount){
 
-        return true;
-    }
-    public boolean traverse(int x,int y,int moveCount){
 
-        board[x][y].visited = true;
-        visitCount[x][y] = moveCount;
-        if(isTourComplete()){
+        if(moveCount==size*size){
             System.out.println("Found a valid tour" + count++);
-            //print();
+            print(visitCount);
             return true;
         }
-        List<Cell> possibleNextMoves = validNextMoves(x,y);
+        List<Pair<Integer,Integer>> possibleNextMoves = validNextMoves(x, y,visitCount);
 
 
-        for(Cell cell : possibleNextMoves){
-
-            if(traverse(cell.x,cell.y,moveCount+1)){
+        for(Pair<Integer,Integer> cell : possibleNextMoves){
+            visitCount[cell.getLeft()][cell.getRight()]=moveCount;
+            if(traverse(cell.getLeft(),cell.getRight(),moveCount+1,visitCount)){
                 return true;
             }
             else {
                 //reset backtrack
-                board[x][y].visited=false;
                 visitCount[x][y] = -1;
             }
         }
@@ -56,15 +39,28 @@ public class HorseMan {
         return false;
     }
 
-    public void print(){
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[i].length;j++){
-                String yes = (board[i][j].visited)? "X" : "";
-                System.out.print(" | " + yes);
-            }
-            System.out.println();
-
+    public void allTours(int x,int y,int moveCount,int[][] visitCount){
+//
+//
+        if(moveCount==size*size){
+            System.out.println("Found a valid tour" + count++);
+            print(visitCount);
+            return;
         }
+        List<Pair<Integer,Integer>> possibleNextMoves = validNextMoves(x, y,visitCount);
+//
+//
+        for(Pair<Integer,Integer> cell : possibleNextMoves){
+               visitCount[cell.getLeft()][cell.getRight()]=moveCount;
+               allTours(cell.getLeft(),cell.getRight(),moveCount+1,visitCount);
+                //reset backtrack
+                visitCount[x][y] = -1;
+            }
+
+    }
+
+
+    public void print(int[][] visitCount){
         System.out.println("---------------------------------------");
         for(int i=0;i<visitCount.length;i++){
             for(int j=0;j<visitCount[i].length;j++){
@@ -79,60 +75,55 @@ public class HorseMan {
 
     public static void main(String[] args){
         HorseMan horseMan = new HorseMan();
-        horseMan.traverse(0,0,1);
+        int[][] visitCount = new int[size][size];
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                visitCount[i][j] = -1;
+            }
+        }
+        visitCount[0][0]=0;
+        horseMan.allTours(0,0,1,visitCount);
 
         System.out.println("Final count" + HorseMan.count);
-        horseMan.print();
+        //horseMan.print(visitCount);
     }
 
-    public List<Cell> validNextMoves(int x,int y){
-         List<Cell> result = new ArrayList<>();
+    public List<Pair<Integer,Integer>> validNextMoves(int x,int y,int[][] visitCount){
 
-        //top left
-        if(x<(size-2) && y<(size-1) && !board[x+2][y+1].visited){
-            result.add(Cell.of(x+2,y+1));
+        List<Pair<Integer,Integer>> result = new ArrayList<>();
+        //bottom right
+        if(x<(size-2) && y<(size-1) && visitCount[x+2][y+1]==-1){
+            result.add(new ImmutablePair<>(x+2,y+1));
         }
-        if(x<(size-1) && y<(size-2) && !board[x+1][y+2].visited){
-            result.add(Cell.of(x+1,y+2));
+        if(x<(size-1) && y<(size-2) && visitCount[x+1][y+2]==-1){
+            result.add(new ImmutablePair<>(x+1,y+2));
         }
         //top right
-        if(x>=2 && y<(size-1) && !board[x-2][y+1].visited){
-            result.add(Cell.of(x-2,y+1));
+        if(x>=2 && y<(size-1) && visitCount[x-2][y+1]==-1){
+            result.add(new ImmutablePair<>(x-2,y+1));
         }
-        if(x>=1 && y<(size-2) && !board[x-1][y+2].visited){
-            result.add(Cell.of(x-1,y+2));
+        if(x>=1 && y<(size-2) && visitCount[x-1][y+2]==-1){
+            result.add(new ImmutablePair<>(x-1,y+2));
         }
-        //bottom right
-        if(x>=2 && y>=1 && !board[x-2][y-1].visited){
-            result.add(Cell.of(x-2,y-1));
+        //top left
+        if(x>=2 && y>=1 && visitCount[x-2][y-1]==-1){
+            result.add(new ImmutablePair<>(x-2,y-1));
         }
-        if(x>=1 && y>=2 && !board[x-1][y-2].visited){
-            result.add(Cell.of(x-1,y-2));
+        if(x>=1 && y>=2 && visitCount[x-1][y-2]==-1){
+            result.add(new ImmutablePair<>(x-1,y-2));
         }
         //bottom left
-        if(x<(size-2) && y>=1 && !board[x+2][y-1].visited){
-            result.add(Cell.of(x+2,y-1));
+        if(x<(size-2) && y>=1 && visitCount[x+2][y-1]==-1){
+            result.add(new ImmutablePair<>(x+2,y-1));
         }
-        if(x<(size-1) && y>=2 && !board[x+1][y-2].visited){
-            result.add(Cell.of(x+1,y-2));
+        if(x<(size-1) && y>=2 && visitCount[x+1][y-2]==-1){
+            result.add(new ImmutablePair<>(x+1,y-2));
         }
 
         return result;
     }
 
 
-}
-class Cell{
-    public final int x,y;
-    public boolean visited= false;
-
-    public Cell(int x,int y){
-        this.x = x;this.y = y;
-
-    }
-    public static Cell of(int x,int y){
-        return new Cell(x,y);
-    }
 }
 
 
