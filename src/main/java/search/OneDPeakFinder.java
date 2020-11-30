8b0ed1814624bb0fecf9efa0a377d2eacfca6c73
sajit <sajit.kunnumkal@gmail.com>
2020-11-29 21:07:09 -0500

    1D peak finder, fast and regular versions

--- src/main/java/search/OneDPeakFinder.java
+++ src/main/java/search/OneDPeakFinder.java
@@ -11,18 +11,40 @@ public class OneDPeakFinder {
         return arr[arr.length-1] > arr[arr.length-2]?arr.length-1:null;
     }
 
+    public static Integer findAPeakV2(Integer[] arr,int startIdx,int endIdx) {
 
+        if(startIdx>=arr.length || endIdx<0){
+            return null;
+        }
+        if(startIdx==arr.length-1){
+            return arr[arr.length-1] > arr[arr.length-2]?arr.length-1:null;
+        }
+        int i = (startIdx+endIdx)/2;
+        if(i>0 && arr[i-1]> arr[i]) {
+            return findAPeakV2(arr,startIdx,i-1);
+        }
+        else if(i<arr.length-1 && arr[i+1]>arr[i]) {
+            return findAPeakV2(arr,i+1,endIdx);
+        }
+        else
+            return null;
+    }
 
 
     public static void main(String[] args) {
         int x = 10_000_000;
         Integer[] arr =new Integer[x];
         for(int i=0;i<arr.length;i++) {
-            arr[i]=i;
+            arr[i]=-i;
         }
         long startTime = System.currentTimeMillis();
         System.out.println("peak index="+ findAPeakV1(arr));
         long endTime = System.currentTimeMillis();
+        System.out.println("Took " + (endTime-startTime) + " milliseconds");
+
+        startTime = System.currentTimeMillis();
+        System.out.println("peak index="+ findAPeakV2(arr,0,arr.length-1));
+        endTime = System.currentTimeMillis();
         System.out.println("Took " + (endTime-startTime) + " milliseconds");
         
     }
