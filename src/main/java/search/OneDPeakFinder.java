c98374d7357ddc94bbe973e7f77610c59cd1c3e4
sajit <sajit.kunnumkal@gmail.com>
2020-12-05 07:04:07 -0500

    1D peak finder, refactor to simplify v1 and v2 edge checks

--- src/main/java/search/OneDPeakFinder.java
+++ src/main/java/search/OneDPeakFinder.java
@@ -1,49 +1,85 @@
 package search;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class OneDPeakFinder {
 
     public static  Integer findAPeakV1(Integer[] arr) {
-        for(int i=1;i<arr.length-1;i++) {
-                if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+        for(int i=1;i<arr.length;i++) {
+                boolean leftCheck = arr[i]>arr[i-1];
+                boolean rightCheck = (i<arr.length-1)?arr[i]>arr[i+1]:true;
+                if( leftCheck && rightCheck){
                     return i;
                 }
         }
-        return arr[arr.length-1] > arr[arr.length-2]?arr.length-1:null;
+        return null;
     }
 
     public static Integer findAPeakV2(Integer[] arr,int startIdx,int endIdx) {
 
-        if(startIdx>=arr.length || endIdx<0){
+        if(endIdx<=0) {
             return null;
         }
+
         if(startIdx==arr.length-1){
             return arr[arr.length-1] > arr[arr.length-2]?arr.length-1:null;
         }
         int i = (startIdx+endIdx)/2;
-        if(i>0 && arr[i-1]> arr[i]) {
-            return findAPeakV2(arr,startIdx,i-1);
+        boolean leftCheck = false;
+        if(i>0){
+            if(arr[i-1] > arr[i]){
+                return findAPeakV2(arr,startIdx,i-1);
+            }
+            else
+                leftCheck = true;
         }
-        else if(i<arr.length-1 && arr[i+1]>arr[i]) {
-            return findAPeakV2(arr,i+1,endIdx);
+        boolean rightCheck = false;
+        if(i<arr.length-1){
+            if(arr[i+1]>arr[i]){
+                return findAPeakV2(arr,i+1,endIdx);
+            }
+            else
+                rightCheck = true;
         }
-        else
-            return null;
+
+        return (leftCheck && rightCheck)?i : null;
+    }
+
+    public static void printArea(Integer idx,Integer[]arr) {
+        if(idx == null){
+            return;
+        }
+        System.out.print("Idx="+idx+ " ");
+        StringBuilder line = new StringBuilder();
+        if(idx>0) {
+            line.append(arr[idx-1]+",");
+        }
+        line.append(arr[idx]);
+        if(idx<arr.length-1){
+            line.append(","+arr[idx+1]);
+        }
+        System.out.println(line.toString());
     }
 
 
     public static void main(String[] args) {
         int x = 10_000_000;
-        Integer[] arr =new Integer[x];
-        for(int i=0;i<arr.length;i++) {
-            arr[i]=-i;
+        List<Integer> arrList = new ArrayList<>();
+        for(int i=0;i<x;i++) {
+            arrList.add(i);
         }
+        Collections.shuffle(arrList);
+        Integer[] arr = new Integer[x];
+        arr = arrList.toArray(arr);
         long startTime = System.currentTimeMillis();
-        System.out.println("peak index="+ findAPeakV1(arr));
+        printArea(findAPeakV1(arr),arr);
         long endTime = System.currentTimeMillis();
         System.out.println("Took " + (endTime-startTime) + " milliseconds");
 
         startTime = System.currentTimeMillis();
-        System.out.println("peak index="+ findAPeakV2(arr,0,arr.length-1));
+        printArea(findAPeakV2(arr,0,arr.length-1),arr);
         endTime = System.currentTimeMillis();
         System.out.println("Took " + (endTime-startTime) + " milliseconds");
         
